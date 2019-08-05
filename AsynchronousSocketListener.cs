57ebe4439032dcd11cbc15f819cbdc2cdcef4456
Tomasz Zajac <zajac.software@gmail.com>
2019-08-05 22:58:22 +0100

    Pushing from archives to git

---
+++ AsynchronousSocketListener.cs
@@ -0,0 +1,261 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bd.tools.net
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading;
+
+    // State object for reading client data asynchronously  
+    public class StateObject
+    {
+        // Client  socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 1024;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public class SocketListenerEventArgs : EventArgs
+    {
+        public const string MESSAGE_RECEIVED = "message_received";
+        public const string SERVER_STERTED = "server_started";
+  
+        public string type { get; set; }
+        public string data { get; set; }
+        public DateTime TimeReached { get; set; }
+    }
+
+    public class AsynchronousSocketListener
+    {
+
+ 
+        public delegate void SocketListenerEventHandler(Object sender, SocketListenerEventArgs e);
+        public event SocketListenerEventHandler SocketEvent;
+
+
+        // Thread signal.  
+        private ManualResetEvent allDone = new ManualResetEvent(false);
+
+        private Thread socetThread;
+
+        private string message = null;
+
+        private const int defaultPort = 11000; 
+
+        public AsynchronousSocketListener()
+        {
+            
+        }
+
+        public void Start()
+        {
+          // StartListening();
+
+            if (socetThread != null)
+            {
+               // throw new Exception("Server's already running!");
+            }
+
+            socetThread = new Thread(StartListening);
+            socetThread.Name = "SocetThread";
+            socetThread.IsBackground = true;
+            socetThread.Start();
+        }
+
+
+        public void NotifyUploadDone()
+        {
+     
+        }
+
+        private void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+         
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            IPAddress ipAddress = null;
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip;
+                    Console.WriteLine("ipadress "+ ipAddress);
+                }
+            }
+
+            if (ipAddress == null)
+            {
+                Console.WriteLine("Local IP adress not found");
+                return;
+            }
+
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+                Console.WriteLine("Socket oppended at:{0}:{1}", localEndPoint.Address.ToString(), AsynchronousSocketListener.defaultPort); 
+                NotifyServerStart(localEndPoint.Address.ToString() + ":" + AsynchronousSocketListener.defaultPort);
+                while (true)
+                {
+                    allDone.Reset();
+     
+                    // Start an asynchronous socket to listen for connections.  
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    
+          
+                    // Wait until a connection is made before continuing.  
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+         ///   Console.WriteLine("\nPress ENTER to continue...");
+          //  Console.Read();
+
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.  
+            allDone.Set();
+
+               // Get the socket that handles the client request.  
+               Socket listener = (Socket)ar.AsyncState;
+                Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.  
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+
+        }
+
+        private void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                // There  might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read   
+                // more data.  
+                content = state.sb.ToString();
+          
+                if (content.IndexOf("<EOF>") > -1)
+                {
+                    // All the data has been read from the   
+                    // client. Display it on the console.  
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}", content.Length, content);
+
+                    // Echo the data back to the client.  
+             
+                    ///  
+              
+                    NotifyMessageReceived(content);
+     
+                    Send(handler, content);
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+
+        }
+   
+        private void NotifyMessageReceived(string data)
+        {
+
+            SocketListenerEventArgs args = new SocketListenerEventArgs();
+            args.type = SocketListenerEventArgs.MESSAGE_RECEIVED;
+            args.data = data;
+            args.TimeReached = new DateTime();
+       
+            if (SocketEvent != null)
+            {
+                SocketEvent(this, args);
+            }
+        }
+
+        private void NotifyServerStart(string data)
+        {
+            SocketListenerEventArgs args = new SocketListenerEventArgs();
+            args.type = SocketListenerEventArgs.SERVER_STERTED;
+            args.data = data;
+            args.TimeReached = new DateTime();
+
+            if (SocketEvent != null)
+            {
+                SocketEvent(this, args);
+            }
+        }
+
+        private void Send(Socket handler, String data)
+        {
+            allDone.Set();
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+    
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+               handler.Shutdown(SocketShutdown.Send);
+               handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using EOSDigital.API;
+using EOSDigital.SDK;
+using bd.tools.net;
+using System.Windows.Threading;
+using System.Net;
+using System.Text;
+namespace DSLR-remote-control
+{
+
+    public partial class MainWindow : Window
+    {
+        #region Variables
+
+        CanonAPI APIHandler;
+        Camera MainCamera;
+        CameraValue[] AvList;
+        CameraValue[] TvList;
+        CameraValue[] ISOList;
+        List<Camera> CamList;
+        bool IsInit = false;
+        int BulbTime = 30;
+        ImageBrush bgbrush = new ImageBrush();
+        Action<BitmapImage> SetImageAction;
+        System.Windows.Forms.FolderBrowserDialog SaveFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+
+        int ErrCount;
+        object ErrLock = new object();
+
+        #endregion
+
+        private AsynchronousSocketListener socetListener;
+        public string newFileName;
+        private string localPath;
+
+    
+
+        public MainWindow()
+        {
+
+            Console.WriteLine(DateTime.Now.ToString("MM-dd-yyyy-HH.mm.ss"));
+         
+            try
+            {
+                InitializeComponent();
+
+                socetListener = new AsynchronousSocketListener();
+                socetListener.SocketEvent += SocetListener_SocketEvent;
+                socetListener.Start();
+
+                APIHandler = new CanonAPI();
+                APIHandler.CameraAdded += APIHandler_CameraAdded;
+                ErrorHandler.SevereErrorHappened += ErrorHandler_SevereErrorHappened;
+                ErrorHandler.NonSevereErrorHappened += ErrorHandler_NonSevereErrorHappened;
+                localPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "RemotePhoto");
+                SavePathTextBox.Text = localPath;
+
+                SetImageAction = (BitmapImage img) => { bgbrush.ImageSource = img; };
+                SaveFolderBrowser.Description = "Save Images To...";
+                RefreshCamera();
+                IsInit = true;
+
+
+            }
+            catch (DllNotFoundException) { ReportError("Canon DLLs not found!", true); }
+            catch (Exception ex) { ReportError(ex.Message, true); }
+        }
+
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+        
+        }
+
+        private string nextQRString = "";
+        private void SocetListener_SocketEvent(object sender, SocketListenerEventArgs e)
+        {
+            switch (e.type)
+            {
+                case SocketListenerEventArgs.MESSAGE_RECEIVED:
+                    Dispatcher.Invoke(new Action(() => {
+                        string[] ar = e.data.Split(':');
+
+                        hostInfo.Text = " data received " + e.data;
+                        if (ar!=null && ar.Length == 3 && ar[0]=="click" && ar[1].Length > 0)
+                        {
+                            nextQRString = ar[1];
+                            takeAhoto();
+                        }
+
+                    }), DispatcherPriority.ContextIdle);
+                    break;
+                case SocketListenerEventArgs.SERVER_STERTED:
+                    Dispatcher.Invoke(new Action(() => {
+                        hostInfo.Text = "Server started. listening on:" + e.data;
+                    }), DispatcherPriority.ContextIdle);
+                    break;
+                default:
+                    //do nothing 
+                    break;
+
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                IsInit = false;
+                MainCamera?.Dispose();
+                APIHandler?.Dispose();
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        #region API Events
+
+        private void APIHandler_CameraAdded(CanonAPI sender)
+        {
+            try { Dispatcher.Invoke((Action)delegate { RefreshCamera(); }); }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void MainCamera_StateChanged(Camera sender, StateEventID eventID, int parameter)
+        {
+
+            try
+            {
+                if (eventID == StateEventID.Shutdown && IsInit)
+                {
+                    Dispatcher.Invoke((Action)delegate { CloseSession(); });
+                }
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void MainCamera_ProgressChanged(object sender, int progress)
+        {
+            try { MainProgressBar.Dispatcher.Invoke((Action)delegate { MainProgressBar.Value = progress; }); }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void MainCamera_LiveViewUpdated(Camera sender, Stream img)
+        {
+            try
+            {
+                using (WrapStream s = new WrapStream(img))
+                {
+                    img.Position = 0;
+                    BitmapImage EvfImage = new BitmapImage();
+                    EvfImage.BeginInit();
+                    EvfImage.StreamSource = s;
+                    EvfImage.CacheOption = BitmapCacheOption.OnLoad;
+                    EvfImage.EndInit();
+                    EvfImage.Freeze();
+                    Application.Current.Dispatcher.BeginInvoke(SetImageAction, EvfImage);
+                }
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+
+        private bool busy = false;
+        private void MainCamera_DownloadReady(Camera sender, DownloadInfo Info)
+        {
+            try
+            {
+                string dir = null;
+                SavePathTextBox.Dispatcher.Invoke((Action)delegate { dir = SavePathTextBox.Text; });
+                //Info.FileName = "local.jpg";
+                string filename;
+                if (nextQRString == "")
+                {
+                    filename = "local_" + DateTime.Now.ToString("MM-dd-yyyy-HH.mm.ss") + ".jpg"; 
+                }
+                else
+                {
+                    filename = nextQRString + ".jpg";
+                }
+
+                Info.FileName = filename;
+
+                sender.DownloadFile(Info, dir);
+                busy = false;
+                nextQRString = "";
+
+                MainProgressBar.Dispatcher.Invoke((Action)delegate { MainProgressBar.Value = 0; });
+
+               
+            }
+            catch (Exception ex)
+            {
+                busy = false;
+            }
+        }
+
+        private void ErrorHandler_NonSevereErrorHappened(object sender, ErrorCode ex)
+        {
+            ReportError($"SDK Error code: {ex} ({((int)ex).ToString("X")})", false);
+        }
+
+        private void ErrorHandler_SevereErrorHappened(object sender, Exception ex)
+        {
+            ReportError(ex.Message, true);
+        }
+
+        #endregion
+
+        #region Session
+
+        private void OpenSessionButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MainCamera?.SessionOpen == true) CloseSession();
+                else OpenSession();
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            try { RefreshCamera(); }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        #endregion
+
+        #region Settings
+
+        private void AvCoBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (AvCoBox.SelectedIndex < 0) return;
+                MainCamera.SetSetting(PropertyID.Av, AvValues.GetValue((string)AvCoBox.SelectedItem).IntValue);
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void TvCoBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (TvCoBox.SelectedIndex < 0) return;
+
+                MainCamera.SetSetting(PropertyID.Tv, TvValues.GetValue((string)TvCoBox.SelectedItem).IntValue);
+                if ((string)TvCoBox.SelectedItem == "Bulb")
+                {
+                    BulbBox.IsEnabled = true;
+                    BulbSlider.IsEnabled = true;
+                }
+                else
+                {
+                    BulbBox.IsEnabled = false;
+                    BulbSlider.IsEnabled = false;
+                }
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void ISOCoBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (ISOCoBox.SelectedIndex < 0) return;
+                MainCamera.SetSetting(PropertyID.ISO, ISOValues.GetValue((string)ISOCoBox.SelectedItem).IntValue);
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void TakePhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            takeAhoto();
+        }
+
+
+        private void takeAhoto()
+        {
+            Console.WriteLine(" takeAhoto ");
+            if (busy)
+            {
+                return;
+            }
+
+            busy = true;
+            try
+            {
+                if ((bool)STComputerRdButton.IsChecked) MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Host);
+                else if ((bool)STBothRdButton.IsChecked) MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Both);
+                MainCamera.SetCapacity(4096, int.MaxValue);
+
+                BrowseButton.IsEnabled = true;
+                SavePathTextBox.IsEnabled = true;
+
+                MainCamera.TakePhoto();
+            }
+            catch (Exception ex)
+            {
+                busy = false;
+              
+            }
+        }
+
+        private void BulbSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try { if (IsInit) BulbBox.Text = BulbSlider.Value.ToString(); }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void BulbBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (IsInit)
+                {
+                    int b;
+                    if (int.TryParse(BulbBox.Text, out b) && b != BulbTime)
+                    {
+                        BulbTime = b;
+                        BulbSlider.Value = BulbTime;
+                    }
+                    else BulbBox.Text = BulbTime.ToString();
+                }
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        private void SaveToRdButton_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (IsInit)
+                {
+                    if ((bool)STCameraRdButton.IsChecked)
+                    {
+                        MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Camera);
+                        BrowseButton.IsEnabled = false;
+                        SavePathTextBox.IsEnabled = false;
+                    }
+                    else
+                    {
+                        if ((bool)STComputerRdButton.IsChecked) MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Host);
+                        else if ((bool)STBothRdButton.IsChecked) MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Both);
+
+                        MainCamera.SetCapacity(4096, int.MaxValue);
+                        BrowseButton.IsEnabled = true;
+                        SavePathTextBox.IsEnabled = true;
+                    }
+                }
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(SavePathTextBox.Text)) SaveFolderBrowser.SelectedPath = SavePathTextBox.Text;
+                if (SaveFolderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    SavePathTextBox.Text = SaveFolderBrowser.SelectedPath;
+                }
+            }
+            catch (Exception ex) { ReportError(ex.Message, false); }
+        }
+
+        #endregion
+
+    
+
+        #region Subroutines
+
+        private void CloseSession()
+        {
+            MainCamera.CloseSession();
+            AvCoBox.Items.Clear();
+            TvCoBox.Items.Clear();
+            ISOCoBox.Items.Clear();
+            SettingsGroupBox.IsEnabled = false;
+            LiveViewGroupBox.IsEnabled = false;
+            SessionButton.Content = "Open Session";
+            SessionLabel.Content = "No open session";
+          
+        }
+
+        private void RefreshCamera()
+        {
+            CameraListBox.Items.Clear();
+            CamList = APIHandler.GetCameraList();
+            foreach (Camera cam in CamList) CameraListBox.Items.Add(cam.DeviceName);
+            if (MainCamera?.SessionOpen == true) CameraListBox.SelectedIndex = CamList.FindIndex(t => t.ID == MainCamera.ID);
+            else if (CamList.Count > 0) CameraListBox.SelectedIndex = 0;
+            OpenSession();
+        }
+
+        private void OpenSession()
+        {
+            if (CameraListBox.SelectedIndex >= 0)
+            {
+                MainCamera = CamList[CameraListBox.SelectedIndex];
+                MainCamera.OpenSession();
+                MainCamera.LiveViewUpdated += MainCamera_LiveViewUpdated;
+                MainCamera.ProgressChanged += MainCamera_ProgressChanged;
+                MainCamera.StateChanged += MainCamera_StateChanged;
+                MainCamera.DownloadReady += MainCamera_DownloadReady;
+   
+
+            SessionButton.Content = "Close Session";
+                SessionLabel.Content = MainCamera.DeviceName;
+                AvList = MainCamera.GetSettingsList(PropertyID.Av);
+                TvList = MainCamera.GetSettingsList(PropertyID.Tv);
+                ISOList = MainCamera.GetSettingsList(PropertyID.ISO);
+                foreach (var Av in AvList) AvCoBox.Items.Add(Av.StringValue);
+                foreach (var Tv in TvList) TvCoBox.Items.Add(Tv.StringValue);
+                foreach (var ISO in ISOList) ISOCoBox.Items.Add(ISO.StringValue);
+                AvCoBox.SelectedIndex = AvCoBox.Items.IndexOf(AvValues.GetValue(MainCamera.GetInt32Setting(PropertyID.Av)).StringValue);
+                TvCoBox.SelectedIndex = TvCoBox.Items.IndexOf(TvValues.GetValue(MainCamera.GetInt32Setting(PropertyID.Tv)).StringValue);
+                ISOCoBox.SelectedIndex = ISOCoBox.Items.IndexOf(ISOValues.GetValue(MainCamera.GetInt32Setting(PropertyID.ISO)).StringValue);
+                SettingsGroupBox.IsEnabled = true;
+                LiveViewGroupBox.IsEnabled = true;
+
+                if ((bool)STComputerRdButton.IsChecked) MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Host);
+                else if ((bool)STBothRdButton.IsChecked) MainCamera.SetSetting(PropertyID.SaveTo, (int)SaveTo.Both);
+                MainCamera.SetCapacity(4096, int.MaxValue);
+                // MessageBox.Show("dupa", "Error", MessageBoxButton.OK, MessageBoxImage.None);
+
+            }
+        }
+
+        private void ReportError(string message, bool lockdown)
+        {
+            int errc;
+            lock (ErrLock) { errc = ++ErrCount; }
+
+            if (lockdown) EnableUI(false);
+
+            if (errc < 4) MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (errc == 4) MessageBox.Show("Many errors happened!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            lock (ErrLock) { ErrCount--; }
+        }
+
+        private void EnableUI(bool enable)
+        {
+            if (!Dispatcher.CheckAccess()) Dispatcher.Invoke((Action)delegate { EnableUI(enable); });
+            else
+            {
+                SettingsGroupBox.IsEnabled = enable;
+                InitGroupBox.IsEnabled = enable;
+                LiveViewGroupBox.IsEnabled = enable;
+            }
+        }
+
+        #endregion
+    }
+}
